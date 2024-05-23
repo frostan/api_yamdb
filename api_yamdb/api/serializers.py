608330bea8b1932c19fa7fc9d08725685680537a
Антон <112401035+frostan@users.noreply.github.com>
2024-05-24 02:31:27 +0300

    Users/urls permissions tokensviews (#4)
    * Набросал 2 модели Comment and Review, а также добавил немного сериализации
    
    * Подкрутил еще модели, просто накидываю код горстями
    
    * Исправил пермишен и сериализаторы и вьюшки
    
    * добавил вью для token и sign
    
    * исправил вью sign
    
    * пофиксил
    
    ---------
    
    Co-authored-by: Sergey Tikhonov <iu-iu@yandex.ru>

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,13 @@
 import datetime as dt
 from rest_framework import serializers
 
-from reviews.models import Categories, Genres, Titles
-#from users.models import CustomUser
+from reviews.models import Categories, Genres, Titles, Review, Comment
+from users.models import CustomUser
+
+# Максимальная оценка.
+MAX_SCORE = 10
+# Минимальная оценка.
+MIN_SCORE = 1
 
 
 class CategoriesSerializers(serializers.ModelSerializer):
@@ -33,7 +38,7 @@ class TitlesPostSerializers(serializers.ModelSerializer):
 
     class Meta:
         model = Titles
-        fields = ('name', 'year', 'description', 'genre', 'category')
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
 
     def validate_year(self, value):
         year = dt.date.today().year
@@ -57,3 +62,63 @@ class TitlesGetSerializers(serializers.ModelSerializer):
             'genre',
             'category'
         )
+
+
+class ReviewSerializers(serializers.ModelSerializer):
+    """Cериализатор ."""
+
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'score', 'title', 'author', 'pub_date')
+
+    def validate_score(self, value):
+        if value in range(MIN_SCORE, MAX_SCORE + 1):
+            raise serializers.ValidationError(
+                f'Оценка выходит за диапазон, {MIN_SCORE}..{MAX_SCORE}')
+        return value
+
+
+class CommentSerializers(serializers.ModelSerializer):
+    """Cериализатор ."""
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        model = Review
+        fields = ('id', 'text', 'author', 'pub_date')
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'bio',
+            'role'
+        )
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email')
+
+    def validate_username(self, username):
+        if username == 'me':
+            raise serializers.ValidationError(
+                ' me нельзя использовать в качестве username'
+            )
+        return username
+
+
+class TokenSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    confirmation_code = serializers.CharField(max_length=250)
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,16 +3,14 @@ from datetime import timedelta
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -22,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'django_filters',
     'reviews.apps.ReviewsConfig',
     'api.apps.ApiConfig',
@@ -60,8 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 
-# Database
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -69,8 +66,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -93,6 +88,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
 }
 
 SIMPLE_JWT = {
@@ -113,9 +110,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
 
