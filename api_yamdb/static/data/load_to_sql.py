469b306a75183de09419d2cd48479be2d52f1301
Антон <frostik96@gmail.com>
2024-05-30 06:39:02 +0300

    load_to_sql correct

--- api_yamdb/static/data/load_to_sql.py
+++ api_yamdb/static/data/load_to_sql.py
@@ -23,22 +23,22 @@ csv_files_names = [
     'users.csv'
 ]
 
-def load_to_sql (dataReader):
+
+def load_to_sql(dataReader):
     line = 0
     for line in dataReader:
-        if line <> 0:
-            category=Category()
-            category.id=row[0]
-            category.review=row[1]
-            category.text=row[2]
-            category.author=row[3]
-            category.pub_date=row[4]
+        if line != 0:
+            category = Category()
+            category.id = row[0]
+            category.review = row[1]
+            category.text = row[2]
+            category.author = row[3]
+            category.pub_date = row[4]
             category.save()
-        else line += 1
+        else:
+            line += 1
+
 
 for file_name in csv_files_names:
     dataReader = csv.reader(open(file_name), delimiter=',')
-    load_to_sql (dataReader)
-
-
-
+    load_to_sql(dataReader)
