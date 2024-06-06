1743f35e8efc38f37cd71729c5e4638bb3cc642f
Антон <frostik96@gmail.com>
2024-06-07 00:46:42 +0300

    Второе ревью

---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,59 @@
+from django.contrib import admin
+
+from reviews.models import Title, Genre, Comment, Review, Category, TitleGenre
+
+
+class GenreInline(admin.TabularInline):
+    model = TitleGenre
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    inlines = GenreInline
+    list_display = (
+        'id',
+        'name',
+        'year',
+        'description',
+        'category'
+    )
+    list_editable = ('category')
+    search_fields = ('year', 'name')
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name'
+    )
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'review',
+        'text',
+        'author',
+        'pub_date'
+    )
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'text',
+        'author',
+        'score',
+        'pub_date'
+    )
