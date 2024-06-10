4a4353441c244a2e8ed14f65eb569f669a8a8d69
Антон <frostik96@gmail.com>
2024-06-10 19:05:55 +0300

    IsAdminOrReadOnly

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -29,6 +29,8 @@ class CommentReviewPermission(BasePermission):
 class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        if request.method in SAFE_METHODS:
-            return True
-        return request.user.is_authenticated and request.user.is_admin
+        return (
+            request.method in SAFE_METHODS
+            or (
+                request.user.is_authenticated
+                and request.user.is_admin))
